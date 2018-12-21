6e0888ab31ce8c198746ae8485bb99e8e24cbb86
yegao <familyink@163.com>
2018-12-21 23:42:10 +0800

    asdas

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
 let esprima = require('esprima');
-let code = 'function ast(){}';
+let estraverse = require('estraverse');
+let escodegen = require('escodegen');
+
+let code = 'let test = ()=>{}';
+// 将代码专程转成成语法树
 let ast = esprima.parse(code);
-console.log(ast)
+console.dir(ast)
+// 遍历抽象语法树
+estraverse.traverse(ast,{
+    enter(node){
+        console.log("enter:",node.type)
+        if(node.type === 'Identifier'){
+            node.name += "_np_";
+        }
+    },
+    leave(node){
+        console.log("leave:",node.type)
+    }
+});
+
+let res = escodegen.generate(ast);
+console.log(res);
